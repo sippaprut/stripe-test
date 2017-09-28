ff975ba0087f75ed3ac18ad0c8171ecb4b36e274
Sippaprut <sippaprut@turkenterprisesinc.com>
2017-09-28 17:36:57 +0700

    test gateway

--- save.php
+++ save.php
@@ -1,33 +1,33 @@
 <?php 
-
+          error_reporting(E_ALL);
 include_once('vendor/autoload.php');
 
 $payment = new Stripe_driver;
 
-print_r($_POST);
-
-// $payment->sk_key = $config['sckey'];
-// $payment->submit_token = getValue( $user_session['checkout']['cc_info']['stripe_token'] );
-// $payment->submit_email = getValue( $user_session['checkout']['cc_info']['stripe_email'] );
 
 
-//             $is_approved = $payment->process( get_charge_amount() );
-//             error_reporting(E_ALL);
-            
-//             devprint($payment->getOrder());
+$payment->sk_key = $config['sckey'];
+$payment->submit_token = $_POST['stripeToken'];
+$payment->submit_email = $_POST['stripeEmail'];
 
-//             $charge_info = array(
-//                 'is_approved' => $is_approved,
-//                 'reason'      => '',
-//                 'result'      => $payment->getOrder()
-//             );
 
-            
+$is_approved = $payment->process( get_charge_amount() );
 
-//             if ( ! $is_approved )
-//             {
-//                 if( is_debugger(get_user_email()) and in_array($ccnum, $test_ccnum)) {
-//                     $charge_info['is_approved'] = true;
-//                     $charge_info['reason'] = 'TEST MODE';
-//                 }
-//             }
+
+print_r($payment->getOrder());
+
+// $charge_info = array(
+//     'is_approved' => $is_approved,
+//     'reason'      => '',
+//     'result'      => $payment->getOrder()
+// );
+
+
+
+// if ( ! $is_approved )
+// {
+//     if( is_debugger(get_user_email()) and in_array($ccnum, $test_ccnum)) {
+//         $charge_info['is_approved'] = true;
+//         $charge_info['reason'] = 'TEST MODE';
+//     }
+// }
